1d65b0541c4200674c1434700c90830e8fc22e16
Borislav2515 <61460569+Borislav2515@users.noreply.github.com>
2021-04-17 14:21:29 +0300

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -1,29 +1,15 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require 'phpmailer/src/PHPMailer.php';
-require 'phpmailer/src/Exception.php';
+$name = $_POST['name'];
+$email = $_POST['email'];
 
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
-$mail->setLanguage('ru', 'phpmailer/language/');
-$mail->IsHTML(true);
+$to = "borislav.khadikov@mail.ru";
+$subject = 'Отправленна заявкаа на тур';
+$message = "Заявка былаа отправлен пользоваателем $name c почтой $email";
+$headers = "Form: $name <$email>" . "\r\n";
 
-$mail->setFrom('Tsallagti.com", Лучшие туры с нами!');
-$mail->addAddress("borislav.khadikov@mail.ru");
-$mail->Subject = "Привет! Я хочу оформить заказ!";
-
-
-$body = '<h1>Тестовое письмо</h1>';
-
-if (trim(!empty(S_POST['name']))) {
-    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
-}
-
-if (!$mail->send()) {
-    $message = 'Ошибка';
+if (mail($to,$subject,$message,$headers)) {
+    echo 'Ваше сообщение успешно отправленно';
 } else {
-    $message = 'Данные отпправились';
+    echo 'Возниклаа ошибка при отправке. Повторите еще рааз';
 }
-$response = ['message' => $message];
